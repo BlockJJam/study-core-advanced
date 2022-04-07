7641f5d025cc320d8453834052825c17cea858c3
blockjjam <blockjjam99@gmail.com>
2022-04-07 22:24:40 +0900

    feat: 필드 동기화로 FieldLogTrace를 만들어 로그 추적기를 구현해보았다 - 문제: 여러쓰레드에서 싱글톤 객체인 FieldLogTrace객체를 동시에 사용해서 로그가 이상하게 찍힌다

---
+++ src/main/java/hello/advanced/app/v3/OrderControllerV3.java
@@ -0,0 +1,32 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.HelloTrace.HelloTraceV2;
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV3 {
+
+    private final OrderServiceV3 orderService;
+    private final LogTrace trace;
+
+    @GetMapping("/v3/request")
+    public String request(String itemId){
+        // 로그 추적기 적용
+        TraceStatus status = null;
+        try{
+            status = trace.begin("OrderController.request()");
+            orderService.orderItem(itemId);
+            trace.end(status);
+            return "OK";
+        }catch(Exception e){
+            trace.exception(status, e);
+            throw e;
+        }
+    }
+
+}
