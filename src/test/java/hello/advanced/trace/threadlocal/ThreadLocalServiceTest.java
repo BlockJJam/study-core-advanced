fe4c726d89994118e72a50e022093187eb8c644f
blockjjam <blockjjam99@gmail.com>
2022-04-09 15:31:35 +0900

    feat: ThreadLocal을 이용한 동시성 문제 해결 및 웹앱 적용 테스트

---
+++ src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,42 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field(){
+        log.info("main start");
+        Runnable userA = () ->{
+            service.logic("userA");
+        };
+        Runnable userB = () ->{
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("Thread-A");// thread name 부여
+        Thread threadB = new Thread(userB);
+        threadB.setName("Thread-B");
+
+        threadA.start();
+        sleep(100); // 동시성 문제가 발생 안함
+        threadB.start();
+        // 여기까지만 돌리면, threadB결과를 스레드에 던지고 main스레드가 종료되어 버림
+
+        sleep(3000); // 원래는 countdown latch사용: 일정 개수의 쓰레드가 모두 끝날 때 까지 기다려야지만 다음으로 진행할 수 있거나 다른 쓰레드를 실행시킬 수 있는 경우 사용
+        log.info("main exit");
+    }
+
+    private void sleep(int millis){
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
